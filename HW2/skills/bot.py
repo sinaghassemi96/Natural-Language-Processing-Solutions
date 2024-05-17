a7cba8ecb5989af764b689d7ff75698a6f87dbb6
s.ghasemi@home.local <sinaghassemi96@gmail.com>
2024-05-17 21:25:01 +0330

    Feat - Finalize Skills

--- HW2/skills/bot.py
+++ HW2/skills/bot.py
@@ -1,12 +1,11 @@
-import sys
 import os
-
-# Add the parent directory of `opsdroid_skills` to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from opsdroid.matchers import match_regex
 from opsdroid.skill import Skill
-from task_extractor.extractor import TaskExtractor
+import imp
+
+dir = os.getcwd()
+extractor = imp.load_source('extractor', dir + '\\HW2\\task_extractor\\extractor.py')
 
 
 def __doc__() -> str:
@@ -25,7 +24,7 @@ class BotSkill(Skill):
 
     def __init__(self, opsdroid, config, *args, **kwargs):
         super().__init__(opsdroid, config, *args, **kwargs)
-        self.extractor = TaskExtractor()
+        self.extractor = extractor.TaskExtractor()
 
     @match_regex(r'command {key}|دستور {key}', case_sensitive=False)
     async def add_event(self, text):
