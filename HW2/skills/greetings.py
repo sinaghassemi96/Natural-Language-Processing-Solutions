c8b84749333b72371fa35b998906905f1b067743
s.ghasemi <sinaghassemi7596@gmail.com>
2024-05-18 19:35:53 +0330

    Feat - Add DB support.

--- HW2/skills/greetings.py
+++ HW2/skills/greetings.py
@@ -1,11 +1,11 @@
 import random
 
-from opsdroid.matchers import match_parse
+from opsdroid.matchers import match_parse, match_regex
 from opsdroid.skill import Skill
 
 
 class HelloSkill(Skill):
-    @match_parse(r'سلام')
+    @match_regex(r'hi')
     async def hello(self, message):
         text = random.choice(
             ["سلام {}", "سلام بر {}", "درود {}", "خوش اومدی {}"]).format(message.user)
